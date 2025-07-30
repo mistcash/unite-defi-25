4b87e687b196244a70477309030c2dcb1103f8d3
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-07-30 19:51:49 +0300

    use default next app

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { StarknetProvider } from "@/components/providers/starknet-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <StarknetProvider>{children}</StarknetProvider>
+        {children}
       </body>
     </html>
   );
