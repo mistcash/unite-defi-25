8923c50b3b076fa357ef676113d241fa6a4243ab
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-08-02 20:34:09 +0300

    contracts refactor

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -3,8 +3,7 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.19",
   paths: {
-    sources: "./contracts",
-    tests: "./test",
+    sources: "./adapters",
     cache: "./cache",
     artifacts: "./artifacts"
   },
